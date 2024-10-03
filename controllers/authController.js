58864506bbbf153c63f9c3b96f87c8efea03f96f
Mashrafie <rahimsheikh05@gmail.com>
2024-10-03 22:13:41 +0600

    Cookie fix

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,6 +15,7 @@ const createSendToken = (user, status, res) => {
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
     ),
     httpOnly: true,
+    sameSite: 'none',
   };
 
   if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
